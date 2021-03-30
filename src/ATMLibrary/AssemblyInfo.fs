64638e7df6d6643cef5bce29a0ae288a3edaf4c5
Alexander Kurdakov <kusancho12@gmail.com>
2021-03-30 23:08:50 +0300

    ---

--- src/ATMLibrary/AssemblyInfo.fs
+++ src/ATMLibrary/AssemblyInfo.fs
@@ -2,8 +2,8 @@
 namespace System
 open System.Reflection
 
-[<assembly: AssemblyTitleAttribute("ninth_homework")>]
-[<assembly: AssemblyProductAttribute("ninth_homework")>]
+[<assembly: AssemblyTitleAttribute("ATMLibrary")>]
+[<assembly: AssemblyProductAttribute("ATMLibrary")>]
 [<assembly: AssemblyVersionAttribute("0.1.0")>]
 [<assembly: AssemblyMetadataAttribute("ReleaseDate","2017-03-17T00:00:00.0000000")>]
 [<assembly: AssemblyFileVersionAttribute("0.1.0")>]
@@ -13,8 +13,8 @@ open System.Reflection
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] AssemblyTitle = "ninth_homework"
-    let [<Literal>] AssemblyProduct = "ninth_homework"
+    let [<Literal>] AssemblyTitle = "ATMLibrary"
+    let [<Literal>] AssemblyProduct = "ATMLibrary"
     let [<Literal>] AssemblyVersion = "0.1.0"
     let [<Literal>] AssemblyMetadata_ReleaseDate = "2017-03-17T00:00:00.0000000"
     let [<Literal>] AssemblyFileVersion = "0.1.0"
