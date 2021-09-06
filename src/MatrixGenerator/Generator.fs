37e68604d38e9e62fa40116bd8a163c150308e56
kusancho <kusancho12@gmail.com>
2021-09-07 05:00:27 +0700

    deleted: coverage report, fixed tests file, rewrited print/generate method

--- src/MatrixGenerator/Generator.fs
+++ src/MatrixGenerator/Generator.fs
@@ -25,15 +25,18 @@ type GeneratorConfig =
 let generateMatrix (config: GeneratorConfig) =
     let rand = Random()
     for number in 0 .. config.amount - 1 do
-        let output = Array2D.zeroCreate config.lines config.cols
-        for i in 0 .. config.lines - 1 do
-            for j in 0 .. config.cols - 1 do
-                let randNum = rand.NextDouble()
-                if randNum > config.sparsity
-                then
-                    output.[i, j] <- match config.dataType with
-                                      | Int -> rand.Next() + 1 |> string // + 1 in that case, when rand.Next() = 0
-                                      | Float -> rand.Next() + 1 |> float |> string
-                                      | Bool -> "1"
-                else output.[i, j] <- "0"
-        printMatrix output <| Path.Combine(config.path, "Matrix" + string number + ".txt")
+        let mtx = seq {
+            for _ in 0 .. config.lines - 1 do
+                seq {
+                    for _ in 0 .. config.cols - 1 ->
+                        let randNum = rand.NextDouble()
+                        if randNum > config.sparsity
+                        then
+                            match config.dataType with
+                                | Int -> rand.Next() + 1 |> string // + 1 in that case, when rand.Next() = 0
+                                | Float -> rand.Next() + 1 |> float |> string
+                                | Bool -> "1"
+                        else "0"
+                        }
+                }
+        printMatrix mtx <| Path.Combine(config.path, "Matrix" + string number + ".txt")
--- src/MatrixGenerator/writePrint.fs
+++ src/MatrixGenerator/writePrint.fs
@@ -4,14 +4,19 @@ module writePrint
 open System.IO
 
 
-let printMatrix (mtx: 't [,]) path =
+let printMatrix (mtx: seq<seq<'t>>) path =
+    let size = Seq.length mtx - 1
+    let writeToFile str =
+        File.AppendAllText(path, str)
+
+    let space cntr =
+        if cntr <> size then " " else ""
+
     if File.Exists path
     then File.Delete path
-    let strMtx = Array2D.map (fun elem -> elem.ToString()) mtx
-    let size = mtx.GetLength(1) - 1
-    for i in 0 .. size - 1 do
-        File.AppendAllText(path, String.concat " " strMtx.[i, *] + "\n")
-    File.AppendAllText(path, String.concat " " strMtx.[size, *])
+    for s in mtx do
+        Seq.iteri (fun i symb -> writeToFile <| (symb.ToString() + space i)) s
+        writeToFile "\n"
 
 
 let readIntMatrix path =
@@ -19,7 +24,7 @@ let readIntMatrix path =
     let colSize = strings.[0].Split(' ').Length
     [|for i in 0 .. strings.Length - 1 ->
       [|
-      let subString = strings.[i].Split(' ')
+      let subString = strings.[i].Split(' ';)
       if subString.Length <> colSize then failwith "wrong matrix"
       for j in 0 .. colSize - 1 ->
           int <| subString.[j]
