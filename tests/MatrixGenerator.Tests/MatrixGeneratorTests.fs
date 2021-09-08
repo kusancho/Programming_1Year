6f2b59fc61841be462b3929ff9bec62b5ca53a86
kusancho <kusancho12@gmail.com>
2021-09-08 17:44:35 +0300

    refactored tests

--- tests/MatrixGenerator.Tests/MatrixGeneratorTests.fs
+++ tests/MatrixGenerator.Tests/MatrixGeneratorTests.fs
@@ -1,5 +1,6 @@
 module MatrixGeneratorTests
 
+open System
 open Expecto
 open Generator
 open writePrint
@@ -7,11 +8,13 @@ open writePrint
 
 let path = __SOURCE_DIRECTORY__ + "/Matrix0.txt"
 
-let sparsity, colSize, lineSize = 0.2, 100, 100
 
-let config = GeneratorConfig(Int, 100, 100, 0.2, 1, __SOURCE_DIRECTORY__)
+let newConfig size =
+    match size with
+    | line, col, sparsity -> GeneratorConfig(Int, line, col, sparsity % 1., 1, __SOURCE_DIRECTORY__)
 
-let newMatrix =
+
+let newMatrix config =
     generateMatrix config
     readIntMatrix path
 
@@ -19,24 +22,27 @@ let newMatrix =
 [<Tests>]
 let testSparseMatrix =
     testList "MatrixGenerator" [
-            testProperty "sparsity" <| fun _ ->
-                let mtx = newMatrix
+            testProperty "line size, col size, sparsity" <| fun (size: int * int * int) ->
+                let lineSize, colSize , sparsity =
+                    match size with
+                    | a, b, c ->
+                        abs a + 100,
+                        abs b + 100,
+                        ((abs c + 3) |> float) * 0.1 % 1. 
+                let mtx = newMatrix <| newConfig (lineSize, colSize, sparsity)
+                let realColSize = mtx.[0].Length
+                let realLineSize = mtx.Length
                 let mutable nons = 0.
                 let numOfElems = float <| mtx.Length * mtx.[0].Length
                 for i in 0 .. mtx.Length - 1 do
                     for j in 0 .. mtx.[0].Length - 1 do
                         if mtx.[i].[j] = 0
                         then nons <- nons + 1.
-                Expect.equal (System.Math.Round(nons / numOfElems, 1)) sparsity ""
 
-            testCase "colSize" <| fun _ ->
-                let mtx = newMatrix
-                let realColSize = mtx.[0].Length
-                Expect.equal realColSize colSize ""
+                Expect.equal (Math.Round(nons / numOfElems, 1)) (Math.Round(sparsity, 1)) "sparsity does not converge"
 
-            testCase "lineSize" <| fun _ ->
-                let mtx = newMatrix
-                let realLineSize = mtx.Length
-                Expect.equal realLineSize lineSize ""
+                Expect.equal realColSize colSize "line size does not converge"
+
+                Expect.equal realLineSize lineSize "col size does not converge"
 ]
 
