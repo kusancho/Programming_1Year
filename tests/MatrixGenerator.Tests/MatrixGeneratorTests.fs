a43ac7e3ab974719651f1e064ca9028b53cd1d9b
kusancho <kusancho12@gmail.com>
2021-09-14 01:57:36 +0300

    refactored tests

--- tests/MatrixGenerator.Tests/MatrixGeneratorTests.fs
+++ tests/MatrixGenerator.Tests/MatrixGeneratorTests.fs
@@ -9,37 +9,33 @@ open writePrint
 let path = __SOURCE_DIRECTORY__ + "/Matrix0.txt"
 
 
-let newConfig size =
-    match size with
-    | line, col, sparsity -> GeneratorConfig(Int, line, col, sparsity, 1, __SOURCE_DIRECTORY__)
+let newConfig lineSize colSize sparsity = GeneratorConfig(Int, lineSize, colSize, sparsity, 1, __SOURCE_DIRECTORY__)
 
 
 let newMatrix config =
     generateMatrix config
     readIntMatrix path
 
+let evaluateSparsity (mtx: int[][]) =
+    let mutable nons = 0.
+    let numOfElems = float <| mtx.Length * mtx.[0].Length
+    for i in 0 .. mtx.Length - 1 do
+        for j in 0 .. mtx.[0].Length - 1 do
+            if mtx.[i].[j] = 0
+            then nons <- nons + 1.
+    nons / numOfElems
+
 
 [<Tests>]
 let testSparseMatrix =
     testList "MatrixGenerator" [
-            testProperty "line size, col size, sparsity" <| fun (size: int * int * int) ->
-                let lineSize, colSize , sparsity =
-                    match size with
-                    | a, b, c ->
-                        abs a + 100,
-                        abs b + 100,
-                        ((abs c + 3) |> float) * 0.1 % 1.
-                let mtx = newMatrix <| newConfig (lineSize, colSize, sparsity)
+            testProperty "line size, col size, sparsity" <| fun (a: int, b: int, c: int) ->
+                let lineSize, colSize, sparsity = abs a + 1000, abs b + 1000, ((abs c + 13) |> float) * 0.1 % 1.
+                let mtx = newMatrix <| newConfig lineSize colSize sparsity
                 let realColSize = mtx.[0].Length
                 let realLineSize = mtx.Length
-                let mutable nons = 0.
-                let numOfElems = float <| mtx.Length * mtx.[0].Length
-                for i in 0 .. mtx.Length - 1 do
-                    for j in 0 .. mtx.[0].Length - 1 do
-                        if mtx.[i].[j] = 0
-                        then nons <- nons + 1.
 
-                Expect.equal (Math.Round(nons / numOfElems, 1)) (Math.Round(sparsity, 1)) "sparsity does not converge"
+                Expect.equal (Math.Round(evaluateSparsity mtx, 2)) (Math.Round(sparsity, 2)) "sparsity does not converge"
 
                 Expect.equal realColSize colSize "line size does not converge"
 
