504f72e7a6e217b28925677f315b231bae856c2f
kusancho <kusancho12@gmail.com>
2021-09-26 16:34:37 +0300

    changed build.sh file

--- build.sh
+++ build.sh
@@ -1,9 +1,25 @@
 #!/usr/bin/env bash
 
+# Reset
+Color_Off='\033[0m'       # Text Reset
+
+# Regular Colors
+Red='\033[0;31m'          # Red
+Cyan='\033[0;36m'         # Cyan
+
 set -eu
 set -o pipefail
 
-echo "Restoring dotnet tools..."
+printf "${Red}[info] ${Cyan}Running 'dotnet tool restore'...${Color_Off}\n"
 dotnet tool restore
 
-PAKET_SKIP_RESTORE_TARGETS=true FAKE_DETAILED_ERRORS=true dotnet fake build -t "$@"
+printf "${Red}[info] ${Cyan}Running 'dotnet paket restore'...${Color_Off}\n"
+dotnet paket restore
+
+printf "${Red}[info] ${Cyan}Running 'dotnet fake build -t \"\$@\" with specified flags'...${Color_Off}\n"
+# Set flags for paket and fake-cli
+PAKET_SKIP_RESTORE_TARGETS=true
+FAKE_DETAILED_ERRORS=true
+
+# Start fake-cli and build repository with targets
+dotnet fake build -t "$@"
