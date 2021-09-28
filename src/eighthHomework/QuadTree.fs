a7a9396acf0d05975ae6f5dc5c523a5cafb9be9c
kusancho <kusancho12@gmail.com>
2021-09-28 14:08:59 +0300

    QuadTree functions

--- src/eighthHomework/QuadTree.fs
+++ src/eighthHomework/QuadTree.fs
@@ -3,6 +3,8 @@ module QuadTree
 
 open AlgebraicStructure
 open SparseMatrix
+open System.Collections.Generic
+open Interfaces
 
 
 type quadTree<'t when 't: equality> =
@@ -63,7 +65,7 @@ type quadTree<'t when 't: equality> =
                      b.scalarMultiply scalar multOp neutral,
                      c.scalarMultiply scalar multOp neutral,
                      d.scalarMultiply scalar multOp neutral).noneCheck neutral
-            | Leaf(a) -> Leaf(multOp scalar a)
+            | Leaf(a) -> Leaf(multOp scalar a).noneCheck neutral
             | None -> None
 
 
@@ -95,6 +97,25 @@ type extendedTree<'t when 't: equality> =
             && this.tree = this.tree
         | _ -> false
 
+    static member private forceCreate (sparseMatrix: SparseMatrix<'t>) =
+        let rec go lineBorder colBorder =
+            if SparseMatrix.isEmptyInPlace sparseMatrix lineBorder colBorder
+            then None
+            else
+                match lineBorder, colBorder with
+                | (a, b), (c, d) when a = b ->  // coordinate of one Cell
+                    Leaf(SparseMatrix.getContent sparseMatrix a c)
+                | (a, b), (c, d) ->
+                    let lineHalf = a + (b - a) / 2
+                    let colHalf = c + (d - c) / 2
+                    Node((go (a, lineHalf) (c, colHalf)),                                 //NW
+                         (go (a, lineHalf) (colHalf + 1, d)),                             //NE
+                         (go (lineHalf + 1, b) (c, colHalf)),                             //SW
+                         (go (lineHalf + 1, b) (colHalf + 1, d)))                         //SE
+
+        let border = sparseMatrix.specSize
+        extendedTree(sparseMatrix.lineSize, sparseMatrix.colSize, (go (0, border - 1) (0, border - 1)))
+
 
     member this.toSparseMatrix =
         let size = this.specSize
@@ -111,6 +132,121 @@ type extendedTree<'t when 't: equality> =
                  (go (lineHalf + 1) lineL (colHalf + 1) colL d))
             | None -> []
         SparseMatrix(this.lineSize, this.colSize, go 0 (size - 1) 0 (size - 1) this.tree)
+
+
+    static member init lineSize colSize (func: int -> int -> 't) =
+        let acc = HashSet<Cell<'t>>()
+        for i in 0 .. lineSize - 1 do
+            for j in 0 .. colSize - 1 do
+                acc.Add <| Cell(i, j, func i j) |> ignore
+        extendedTree.forceCreate (SparseMatrix(lineSize, colSize, List.ofSeq acc))
+
+
+    static member clearNeutral neutral (tree: extendedTree<'t>) =
+        let rec go tree =
+            match tree with
+            | Node(a, b, c, d) -> Node(go a, go b, go c, go d).noneCheck neutral
+            | Leaf(a) -> Leaf(a).noneCheck neutral
+            | None -> None
+        extendedTree(tree.lineSize, tree.colSize, go tree.tree)
+
+
+    member this.fillNeutral (neutral: 't) =
+        let content = this.toSparseMatrix.content
+        let flagLst = [for item in content -> (item.line, item.col)]
+        let contWithNeutrals = [for i in 0 .. this.lineSize - 1 do
+                                    for j in 0 .. this.colSize - 1 do
+                                        if not <| List.contains (i, j) flagLst
+                                        then Cell(i, j, neutral)]
+        extendedTree.forceCreate <| SparseMatrix(this.lineSize, this.colSize, List.concat [contWithNeutrals; content])
+
+
+    member this.getByIndex i j algStr =
+        let neutral =
+            match algStr with
+            | Monoid x -> x.Neutral
+            | SemiRing x -> x.Monoid.Neutral
+        let size = this.specSize
+        if this.lineSize <= i || this.colSize <= j || i < 0 || j < 0
+        then failwith "Index was outside the bounds of the array."
+        let rec go lineH lineL colH colL tree =
+            match tree with
+            | Leaf(a) -> a
+            | Node(a, b, c, d) ->
+                let lineHalf = lineH + (lineL - lineH) / 2
+                let colHalf = colH + (colL - colH) / 2
+                match i, j with
+                | i, j when i <= lineHalf && j <= colHalf -> go lineH lineHalf colH colHalf a
+                | i, j when i <= lineHalf && j > colHalf -> go lineH lineHalf (colHalf + 1) colL b
+                | i, j when i > lineHalf && j <= colHalf -> go (lineHalf + 1) lineL colH colHalf c
+                | i, j when i > lineHalf && j > colHalf -> go (lineHalf + 1) lineL (colHalf + 1) colL d
+                | _ -> failwith "Index was outside the bounds of the array."
+            | None -> neutral
+        go 0 (size - 1) 0 (size - 1) this.tree
+
+
+    static member iteri (func: int -> int -> 't -> unit) (tree: extendedTree<'t>) =
+        let size = tree.specSize
+        let rec go lineH lineL colH colL tree =
+            match tree with
+            | Leaf(a) ->
+                func lineH colH a
+            | Node(a, b, c, d) ->
+                let lineHalf = lineH + (lineL - lineH) / 2
+                let colHalf = colH + (colL - colH) / 2
+                (go lineH lineHalf colH colHalf a)
+                (go lineH lineHalf (colHalf + 1) colL b)
+                (go (lineHalf + 1) lineL colH colHalf c)
+                (go (lineHalf + 1) lineL (colHalf + 1) colL d)
+            | None -> ()
+        go 0 (size - 1) 0 (size - 1) tree.tree
+
+
+    /// not enough safe, possible situation:
+    /// Node(None, None, None, Leaf(1)) -> Node(None, None, None, Leaf(0))
+    /// expected: None
+    static member mapi (func: int -> int -> 't -> 'a) (exTree: extendedTree<'t>) =
+        let size = exTree.specSize
+        let rec go lineH lineL colH colL tree =
+            match tree with
+            | Leaf(a) -> Leaf(func lineH colH a)
+            | Node(a, b, c, d) ->
+                let lineHalf = lineH + (lineL - lineH) / 2
+                let colHalf = colH + (colL - colH) / 2
+                Node((go lineH lineHalf colH colHalf a),
+                     (go lineH lineHalf (colHalf + 1) colL b),
+                     (go (lineHalf + 1) lineL colH colHalf c),
+                     (go (lineHalf + 1) lineL (colHalf + 1) colL d))
+            | None -> None
+        extendedTree(exTree.lineSize, exTree.colSize, go 0 (size - 1) 0 (size - 1) exTree.tree)
+
+
+    /// application func to non None elements
+    static member fold (func: 'a -> 't -> 'a) (acc: 'a) (tree: extendedTree<'t>) =
+        let rec go tree acc =
+            match tree with
+            | Node(a, b, c, d) ->
+                go d (go c (go b (go a acc)))
+            | Leaf(a) -> func acc a
+            | None -> acc
+        go tree.tree acc
+
+
+    /// application func to non None elements
+    static member map (func: 't -> 'a) (exTree: extendedTree<'t>) =
+        let size = exTree.specSize
+        let rec go lineH lineL colH colL tree =
+            match tree with
+            | Leaf(a) -> Leaf(func a)
+            | Node(a, b, c, d) ->
+                let lineHalf = lineH + (lineL - lineH) / 2
+                let colHalf = colH + (colL - colH) / 2
+                Node((go lineH lineHalf colH colHalf a),
+                     (go lineH lineHalf (colHalf + 1) colL b),
+                     (go (lineHalf + 1) lineL colH colHalf c),
+                     (go (lineHalf + 1) lineL (colHalf + 1) colL d))
+            | None -> None
+        extendedTree(exTree.lineSize, exTree.colSize, go 0 (size - 1) 0 (size - 1) exTree.tree)
 
 
     member this.plus (y: extendedTree<'t>) (algStruct: AlgebraicStruct<'t>) =
@@ -197,16 +333,23 @@ type extendedTree<'t when 't: equality> =
                 Node((go a), (go b), (go c), (go d)).noneCheck neutral
             | Leaf a -> (snd.tree.scalarMultiply a multOp neutral).noneCheck neutral
             | None -> None
-        extendedTree(this.lineSize * snd.colSize, this.colSize * snd.colSize, go this.tree)
+
+        let lineDeviation = snd.specSize - snd.lineSize
+        let colDeviation = snd.specSize - snd.colSize
+        let tempSparse = extendedTree(this.lineSize * snd.specSize, this.colSize * snd.specSize, go this.tree).toSparseMatrix
+        let cells = List.map (fun (x: Cell<'t>) -> Cell(x.line - (x.line / snd.specSize) * lineDeviation,
+                                                    x.col - (x.col / snd.specSize) * colDeviation,
+                                                    x.data)) tempSparse.content
+        extendedTree.createTreeOfSparseMatrix algStruct (SparseMatrix(this.lineSize * snd.lineSize, this.colSize * snd.colSize, cells))
 
 
-    member private this.toBoolTree (exTree: extendedTree<'t>) =
+    member this.toBoolTree =
         let rec go tree =
             match tree with
             | Node(a, b, c, d) -> Node(go a, go b, go c, go d)
-            | Leaf a -> Leaf 1
+            | Leaf _ -> Leaf true
             | None -> None
-        extendedTree(exTree.lineSize, exTree.colSize, go exTree.tree)
+        extendedTree(this.lineSize, this.colSize, go this.tree)
 
 
     member this.transitiveClosure (algStruct: AlgebraicStruct<'t>) =
@@ -222,4 +365,40 @@ type extendedTree<'t when 't: equality> =
                 let temp =  this.multiply curr semiRing
                 accumulate temp (iter - 1) (acc @ [temp])
         let lstOfMtx = accumulate this n []
+
         List.fold (fun (acc: extendedTree<_>) x -> acc.plus x monoid) this lstOfMtx
+
+
+    interface IMatrix<'t> with
+        member this.colSize =
+            this.colSize
+
+        member this.fold acc func =
+            extendedTree.fold acc func this
+
+        member this.get(i, j, algStr) =
+            this.getByIndex i j algStr
+
+        member this.iteri func =
+            extendedTree.iteri func this
+
+        member this.lineSize =
+            this.lineSize
+
+        member this.map func =
+            extendedTree.map func this :> IMatrix<_>
+
+        member this.mapi func =
+            extendedTree.mapi func this :> IMatrix<_>
+
+        member this.tensorMultiply (snd: IMatrix<'t>) algStr =
+            match snd with
+            | :? extendedTree<'t> as x ->
+                this.tensorMultiply x algStr :> IMatrix<_>
+            | _ -> failwith " "
+
+        member this.toBool neutral =
+            (extendedTree.clearNeutral neutral this).toBoolTree :> IMatrix<_>
+
+        member this.transitiveClosure algStr =
+            this.transitiveClosure algStr :> IMatrix<_>
