2f9e1448c79ae93e07fd45a31ca036fe91402a7e
kusancho <kusancho12@gmail.com>
2021-11-10 04:41:25 +0300

    build problems

--- src/Comparison/Main.fs
+++ src/Comparison/Main.fs
@@ -7,10 +7,4 @@ module Main =
 
     [<EntryPoint>]
     let main (argv: string array) =
-        let semiRing = SemiRing (new SemiRing<int>(new Monoid<int>((+), 0), (*)))
-        let multSeq (fst: extendedTree<_>) snd = fst.multiply snd semiRing
-        let sparsity = 0.8
-        let multParallel depth (fst: extendedTree<_>) snd = fst.parallelMultiply snd semiRing depth
-        Comparison.createCSV (__SOURCE_DIRECTORY__ + $"/Plots/seq{sparsity}.csv") (__SOURCE_DIRECTORY__ + $"/Plots/parallel{sparsity}.csv")
-                   multSeq (multParallel 1) 7000 45000 1000 3 sparsity
         0
